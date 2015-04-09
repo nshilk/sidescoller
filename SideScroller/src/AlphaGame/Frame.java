559c1e61f8139241af318d7de431fc04e0e21c7d
ttran9619 <ttran9619@gmail.com>
2015-04-09 15:04:36 -0500

    Added Nathan's Block Code

--- SideScroller/src/AlphaGame/Frame.java
+++ SideScroller/src/AlphaGame/Frame.java
@@ -1,76 +1,26 @@
 package AlphaGame;
 import java.io.FileNotFoundException;
 
-
-import java.util.Timer;
-
-
 /** 
  *   The Main Class for the test game
  *   
  *   @author Team Two
  */
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Frame {
-	
-	static JFrame frame = new JFrame("2D Game");
-	static TitleScreen title;
-	static Background level;
-	
-	static int maxScore;
 
 	public Frame() throws FileNotFoundException{
-		maxScore=0;
-		frame.add(title = new TitleScreen());
-		
+		JFrame frame = new JFrame("2D Game");
+		frame.add(new Background());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 448);
 		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);	
-		
-		
-	}
-	
-	public static void setScore(int score){
-		maxScore = score;
-	}
-	public static int getScore(){
-		return maxScore;
+		frame.setLocationRelativeTo(null);
+		//TEST?
 	}
 
-	public static void lvlEnd() throws FileNotFoundException{
-		frame.getContentPane().removeAll();
-		
-		frame.add(title = new TitleScreen());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.repaint();
-	}
-	
-	
-	public static void lvlStart(String name) throws FileNotFoundException{
-		frame.getContentPane().removeAll();
-		
-		frame.add(level=new Background(name));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.repaint();
-		
-	}
 	public static void main(String[] args) throws FileNotFoundException{
-		
-		
-		
-		Frame game =new Frame();
-		
-		
+		new Frame();
 	}
-
-	
-	
-
 }
